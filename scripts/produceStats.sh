b5cc84a9207c58f5c1ab1a4afabd2ebd0670f3e0
Stanley Gan <glgan@sfu.ca>
2017-05-11 14:56:09 -0700

    Python output folder option

--- scripts/produceStats.sh
+++ scripts/produceStats.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
-sample=/home/glgan/Documents/Borrelia/Test_Data/SRR2034333/
+sample=/home/glgan/Documents/Borrelia/Test_Data/SRR2034333_old/
 files=`ls $sample`
 
 for f in $files
 do
 	echo $f
 	cd $sample$f
-	python ~/Documents/Borrelia/scripts/Compute_values.py -g $f -l 41 > ~/Documents/Borrelia/$1/$f\_output_stats.txt
+	python ~/Documents/Borrelia/scripts/Compute_values.py -g $f -l 41 > ~/Documents/Borrelia/$1/$f\_output_stats.txt -o $1
 done	
