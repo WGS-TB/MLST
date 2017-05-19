8b0572efdf27817814e17b5a166bb645c8fb93f1
Stanley Gan <glgan@sfu.ca>
2017-05-18 20:53:46 -0700

    Fixed bug in matrix generation, added likelihood function, ilp.solver returns objective values

--- scripts/pipeline_wrapper.py
+++ scripts/pipeline_wrapper.py
@@ -13,6 +13,7 @@ import csv
 import variantILP as ilp
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 #Generate matrix of rows=reads and columns=number of mismatches
 def Generate_Matrix(path):
@@ -35,6 +36,12 @@ def Generate_Matrix(path):
 
             for i in range(len(read_list)):
                     num_mismatch = mismatch_list[i].count('>') #count the number of mismatches for each read
+                    
+                    if  i%2 == 0:    
+                        read_list[i] = read_list[i]+'-1/2'
+                    else:
+                        read_list[i] = read_list[i]+'-2/2'
+                    
                     d[read_list[i]].append(var_list[i]) #append all the variants that read read_i maps to
                     d_1[read_list[i]].append(num_mismatch) #append all the mismatches that each read_i has when it maps to a variants
                     d_2[read_list[i]].append(i) #for testing purposes
@@ -82,11 +89,11 @@ def compute_proportions(dataframe):
                             temp_list[i] = 0
             total = sum(temp_list)
             #solve for k
-            temp_list = [j*(1.0/total) for j in temp_list]
+            temp_list = [j*(100000000.0/(100000000*total)) for j in temp_list]
             prob_list.append(temp_list)
     col_sums = [sum(k) for k in zip(*prob_list)]
     total_sum = sum(col_sums)
-    prop_list = [l*(1/total_sum) for l in col_sums]
+    prop_list = [100.0*l*(1/total_sum) for l in col_sums]
     return prop_list    
 
 def create_dictionary(keys, vals):
@@ -96,6 +103,30 @@ def create_dictionary(keys, vals):
                     my_dict[keys[i]] = vals[i]*100
     return my_dict 
 
+def compute_likelihood(df):
+    numVar = df.shape[1]
+    likelihood_list = list()
+    max_mm = 2
+    
+    for row in df.itertuples(index=False):
+        read = list(row)
+        
+        temp = list()
+        for i in range(numVar):
+            if read[i] == -1:
+                prob = (0.01)**(max_mm+1) * (0.99)**(76 - max_mm -1)
+                temp.append(prob)
+            else:
+                prob = (0.01)**(read[i]) * (0.99)**(76 - read[i])
+                temp.append(prob)
+                
+        likelihood_list.append( sum(temp) )
+    
+    likelihood_list = [i/(2.0*76*numVar) for i in likelihood_list]
+    neg_log_likelihood = [-1.0*np.log10(j) for j in likelihood_list]
+    
+    score = sum(neg_log_likelihood)
+    return score
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--path", required = True, help = "Path to data table")
@@ -108,7 +139,7 @@ path = args["path"]
 df = Generate_Matrix(path)
 df.rename(columns={'Unnamed: 0': 'Read'}, inplace=True)
 #predict variants
-pred_object_val,var_predicted,reads_cov, all_solutions = ilp.solver(df)
+pred_object_val,var_predicted,reads_cov, all_solutions, all_objective = ilp.solver(df)
 #print var_predicted
 #print all_solutions
 df2 = df.loc[reads_cov,var_predicted]
@@ -122,10 +153,31 @@ fig.savefig(args["gene"]+'_matrix_plots.png')
 #compute proportions
 prop = compute_proportions(df2)
 pred_prop = create_dictionary(var_predicted, prop)
-print len(all_solutions)
-for sol in all_solutions:
-    print sol
-    print(compute_proportions(df.loc[reads_cov, sol]))
+
+#score list and proportions
+score_list = list()
+min_score = sys.maxint
+min_sol_list = list()
+print("Total number of optimal solutions: {}".format(len(all_solutions)))
+
+for i in range(len(all_solutions)):
+    print("Solution: {}".format(all_solutions[i]))
+    print("Objective value: {}".format(all_objective[i]))
+    print("Proportion: {}".format(compute_proportions(df.loc[reads_cov, all_solutions[i]])))
+    score = compute_likelihood(df.loc[reads_cov, all_solutions[i]])
+    score_list.append(score)
+    
+    if score <= min_score:
+        min_score = score
+    
+    print("Negative log likelihood score:{}\n".format(score))
+    
+min_sol_list = [all_solutions[i] for i in range(len(all_solutions)) if score_list[i] == min_score]
+    
+print("**Summary**")
+print("Negative log likelihood score list:{}".format(score_list))
+print("Minimum negative log likelihood score: {}".format(min_score))
+print("Solution(s) which have minimum negative log likelihood: {}".format(min_sol_list))
 
 #write proportions to file
 w = csv.writer(open(args["gene"]+'_proportions.csv', "w"))
