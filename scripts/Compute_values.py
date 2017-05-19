1e75d351d0ffed8080745e3d7eed8488f0be99d9
ecool50 <ewillie@sfu.ca>
2017-05-19 00:58:59 -0700

    Changed some variables names and added more comments to Compute_values.py

--- scripts/Compute_values.py
+++ scripts/Compute_values.py
@@ -67,42 +67,45 @@ def Generate_Matrix(path):
                 read_list.append(parts[0]) #append reads to a list
                 var_list.append(parts[1])   #append vars to a list
                 mismatch_list.append(parts[2]) #append mismatches to list
-        flag = True
+        flag = True #makes sure all the previous steps completed successfully
     if flag is True:
-        d = defaultdict(list) #dictionary holding all the variants that a read maps to
-        d_1 = defaultdict(list) #dictionary holding all the mismatches that a read has to its variants
+        read_var_dict = defaultdict(list) #dictionary holding all the variants that a read maps to
+        read_mismatch_dict = defaultdict(list) #dictionary holding all the mismatches that a read has to its variants
         d_2 = defaultdict(list) #dictionary holding indices for later use
 
 
         for i in range(len(read_list)):
             num_mismatch = mismatch_list[i].count('>') #count the number of mismatches for each read
-            if  i%2 == 0:    
+            #append the appropraite suffix for paired reads
+            '''if  i%2 == 0:    
                 read_list[i] = read_list[i]+'-1/2'
             else:
                 read_list[i] = read_list[i]+'-2/2'
-            d[read_list[i]].append(var_list[i]) #append all the variants that read read_i maps to
-            d_1[read_list[i]].append(num_mismatch) #append all the mismatches that each read_i has when it maps to a variants
+            '''
+            read_var_dict[read_list[i]].append(var_list[i]) #append all the variants that read read_i maps to
+            read_mismatch_dict[read_list[i]].append(num_mismatch) #append all the mismatches that each read_i has when it maps to a variants
             d_2[read_list[i]].append(i) #for testing purposes
 
-        var_list = set(var_list)
-        x = tree()
+        var_list = set(var_list) #removes duplicates
+        matrix_dict = tree() #creates a 2-D dictionary object later used to generate the read-variant matrix datastructure
  
 #       create a 2D dictionary that contains all the possible combinations of a read with a variant and the number of mismatches.
         for var in var_list:
-            for key in d:   #key=read name
-                temp = d[key]   #list of variants that key maps to 
-                if var in temp:
-                    index = temp.index(var)
-                    val = d_1[key][index]
+            for read in read_var_dict:   #key=read name
+                temp_var_list = read_var_dict[read]   #list of variants that key maps to 
+                if var in temp_var_list:
+                    index = temp_var_list.index(var)
+                    mismatch = read_mismatch_dict[read][index] #get the number of mismatches
                     #only for perfect matches.
                     #if val == 1:
                     #print val
-                    x[key][var] = int(val)
+                    matrix_dict[read][var] = int(mismatch) #add it to the matrix data structure
 
-        df = DataFrame(x).T.fillna(-1)
+        df = DataFrame(matrix_dict).T.fillna(-1) #convert 2-D dictionary to a matrix
     return df
 
 def compute_probability(n, k):
+    #compute the probability of read of length n mapping to a variant with k mismatches using the binomial distribution
     if NO_BINOM:
         b=10**10
     else:
@@ -115,6 +118,7 @@ def compute_probability(n, k):
     return prob
 
 def compute_proportions(dataframe):
+    #computes the proportion of a set of variants given a set of reads uing probabilistic methods
         prob_list = [] #a list to hold the probabilities
         for row in dataframe.itertuples(index=False):
                 temp_list = list(row)
@@ -191,7 +195,7 @@ def compute_likelihood(df):
 ap = argparse.ArgumentParser()
 ap.add_argument("-g", "--gene", required = True,  help="name of gene")
 ap.add_argument("-l", "--numOfIter", required = False, default = 40, type=int)
-ap.add_argument("-o", "--outputFolder", required=True)
+#ap.add_argument("-o", "--outputFolder", required=True)
 args = vars(ap.parse_args())
 random.seed(a=1994) #set random seed
 true_ratios = []
@@ -208,149 +212,147 @@ minNegLogLike_correct = 0
 minSizeOpt_count = 0
 minNegLogLike_hasTrue = 0
 
-for x in range(1,args["numOfIter"]+1): 
-        k =random.randint(2,7) #generate a random integer k between 2 and 7
-	#generate k random fractions that sum up to 1
-        r = [random.random() for j in range(k)] 
-        s = sum(r)
-        r = [ i/s for i in r ]
-	#run a bash sub command to sort the variants text file and return the firt k variants
-        variants = (sh.head(sh.sort("variants.txt", "-R"), "-n", k))
-        variants = list(variants) #convert output from runningCommand to a list
-        #print variants #for testing purposes
-        ratios = [] #list to store the proprotions of reads to generate
-        true_prop = dict()
-	gene = args["gene"]
-	gene = upperfirst(gene)
-	temp2 = ""
-	file_name2 = gene+"_"+str(x)+".fas"
-        variants_current = [] #list to hold the variants randomly chosen
-        for i in range(len(r)):
-                
-                string = str(variants[i]) 
-                string = string.rstrip() #remove the "\n" character that is returned by bash
-                string1= string.split(">")
-		#print string1
-                variants_current.append(string1[1]) #append the variant to the list
-                sim_name = string1[1]+"_"+str(x)+"_"
-                file_name = sim_name+"reference.fa"
-                val = math.ceil(r[i]*197) #compute the proportions 
-                ratios.append(int(val)) 
-                temp=sh.grep(string,"linear.txt","-w","-A1") #use bash to extract the variant sequence
-		temp = temp.rstrip()
-                temp = str(temp)
-		temp2 = temp2 + temp
-		#print temp2 
-                cmd = "art_illumina -q -ss HS25 -sam -i "+file_name+" -p -l 76 -c "+str(ratios[i])+" -m 200 -s 10 -o "+sim_name + " >/dev/null"#the ART command to generate the simulated data for a variant
-                #write the variant sequence to a text file
-		with open(file_name, "w") as text_file: 
-                        text_file.write(temp)
+for x in range(1,args["numOfIter"]+1):  
+    k =random.randint(2,7) #generate a random integer k between 2 and 7
+    #generate k random fractions that sum up to 1
+    fractions = [random.random() for j in range(k)] 
+    s = sum(fractions)
+    fractions = [ i/s for i in fractions ]
+	#run a bash sub command to sort the variants text file and return the first k variants
+    variants = (sh.head(sh.sort("variants.txt", "-R"), "-n", k))
+    variants = list(variants) #convert output from runningCommand to a list
+    #print variants #for testing purposes
+    ratios = [] #list to store the proprotions of reads to generate
+    true_prop = dict()#dictionary to store the true proportions
+    gene = args["gene"] #get the locus/gene of interest
+    gene = upperfirst(gene)
+    total_variant_seqs = "" #string for all the current variant sequences
+    file_name2 = gene+"_"+str(x)+".fas"
+    variants_current = [] #list to hold the variants randomly chosen
+    for i in range(len(fractions)):        
+        variant = str(variants[i]) 
+        variant = variant.rstrip() #remove the "\n" character that is returned by bash
+        string1= variant.split(">")
+        variants_current.append(string1[1]) #append the variant to the list
+        sim_name = string1[1]+"_"+str(x)+"_" #name of simulation
+        file_name = sim_name+"reference.fa"
+        num_reads = math.ceil(fractions[i]*197) #compute the number of reads to generate for each variant
+        ratios.append(int(num_reads)) 
+        variant_seq = sh.grep(variant,"linear.txt","-w","-A1") #use bash to extract the variant sequence
+        variant_seq = variant_seq.rstrip() #remove the "\n" character that is returned by bash
+        variant_seq = str(variant_seq)
+        total_variant_seqs = total_variant_seqs + variant_seq
+        #print temp2 
+        cmd = "art_illumina -q -ss HS25 -sam -i "+file_name+" -p -l 76 -c "+str(ratios[i])+" -m 200 -s 10 -o "+sim_name + " >/dev/null"#the ART command to generate the simulated data for a variant
+        #write the variant sequence to a text file
+        with open(file_name, "w") as text_file: 
+            text_file.write(variant_seq)
                 #print sim_name #testing purposes
                 #print file_name #testing purposes
                 #print cmd #testing purposes
-                os.system(cmd) #run the ART command
-	with open(file_name2, "w") as text_file2:
-		text_file2.write(temp2)
-        new_cmd = "cat *_1.fq > "+gene+"_"+str(x)+"_1.fa" #append all the first of the pairs together
-        new_cmd2 ="cat *_2.fq > "+gene+"_"+str(x)+"_2.fa" #append all the second of the pairs together
-        ref = upperfirst(gene)+"_bowtie"
-	new_cmd3 = "bash /home/glgan/Documents/Borrelia/scripts/temp.sh "+ gene+"_"+str(x) + " " + gene + "_" + str(x)+ " " + ref + " >/dev/null "
-	#print new_cmd3 
-        os.system(new_cmd) #run the command
-        os.system(new_cmd2) #run the command
-	os.system(new_cmd3)
-        os.system("rm "+args["gene"]+"*") #remove unneccessary files for the next iteration.
-        #print ratios #for testing purposes
-        X =  gene+"_"+str(x)
-        X = upperfirst(X)
-        #print X
+        os.system(cmd) #run the ART command
+    with open(file_name2, "w") as text_file2:
+		text_file2.write(total_variant_seqs)
+    new_cmd = "cat *_1.fq > "+gene+"_"+str(x)+"_1.fa" #append all the first of the pairs together
+    new_cmd2 ="cat *_2.fq > "+gene+"_"+str(x)+"_2.fa" #append all the second of the pairs together
+    ref = upperfirst(gene)+"_bowtie"
+    new_cmd3 = "bash /home/elijah/Desktop/SRA_bowtie/Alignments/scripts/temp.sh "+ gene+"_"+str(x) + " " + gene + "_" + str(x)+ " " + ref + " >/dev/null "
+    #print new_cmd3 
+    os.system(new_cmd) #run the command
+    os.system(new_cmd2) #run the command
+    os.system(new_cmd3)
+    os.system("rm "+args["gene"]+"*") #remove unneccessary files for the next iteration.
+    #print ratios #for testing purposes
+    X =  gene+"_"+str(x)
+    X = upperfirst(X)
+    #print X
+    
+    true_ratios.append(fractions)
+    true_variants.append(variants_current)
+    for j in range(0,len(variants_current)):
+            key = variants_current[j]
+            true_prop[key] = float(fractions[j])*100
+    
+    path = X+'_reads.txt'
+    df = Generate_Matrix(path)
+    df.rename(columns={'Unnamed: 0': 'Read'}, inplace=True)
+    pred_object_val,var_predicted,reads_cov,all_solutions, all_objective = ilp.solver(df)
+    
+    #If there is one solution among all optimal which matches true variants, assign to var_predicted
+    for solution in all_solutions:
+        if set(solution) == set(variants_current):
+            var_predicted = solution
+            break
+    
+    Precision.append(precision(var_predicted, variants_current))
+    Recall.append(recall(var_predicted, variants_current))
+    pred_object_vals.append(pred_object_val)
+    df1 = df[var_predicted]
+    df2 = df.loc[reads_cov,variants_current]
+    df3 = df.loc[reads_cov,var_predicted]
+    prop = compute_proportions(df3)
+    pred_prop = create_dictionary(var_predicted, prop)
+    val = totalVariationDist(pred_prop, true_prop)
+    total_var.append(val)
+    print "======================================== SIMULATION " + str(x) + " ====================================================" +  "\n"
+    print 'True variants are:', variants_current, "\n"
+    print 'Predicted variants are:', var_predicted, "\n"
+    print 'True proportions are:', true_prop, "\n"
+    print 'Predicted proportions are:', pred_prop, "\n"
+    #print 'Solution(s):', all_solutions, "\n"
+    
+    #Likelihood approach
+    score_list = list()
+    min_score = sys.maxint
+    print("Number of optimal solutions: {}".format(len(all_solutions)))
+    
+    #compute negative log likelihood score for each solution
+    for i in range(len(all_solutions)):
+        print("Solution:{}".format(all_solutions[i]))
+        print("Objective value: {}".format(all_objective[i]))
+        print("Proportions:{}".format(compute_proportions(df.loc[reads_cov, all_solutions[i]])))
+        score = compute_likelihood(df.loc[reads_cov, all_solutions[i]])
+        score_list.append(score)
         
-        true_ratios.append(r)
-        true_variants.append(variants_current)
-        for j in range(0,len(variants_current)):
-                key = variants_current[j]
-                true_prop[key] = float(r[j])*100
+        if score <= min_score:
+            min_score = score
         
-        path = X+'_reads.txt'
-        df = Generate_Matrix(path)
-        df.rename(columns={'Unnamed: 0': 'Read'}, inplace=True)
-        pred_object_val,var_predicted,reads_cov,all_solutions, all_objective = ilp.solver(df)
+        print("\nNegative log likelihood score:{}\n".format(score))
+    
+    #identify those solutions which have minimum negative log likelihood
+    min_sol_list = [all_solutions[i] for i in range(len(all_solutions)) if score_list[i] == min_score]
+    min_sol = min_sol_list[0]
+    
+    #Observe whether minimum negative log likelihood really gives the true variants
+    for sol in min_sol_list:
+        if set(sol) == set(variants_current):
+            minNegLogLike_correct += 1
+            min_sol = sol
+            break
+    
+    #Observe whether minimum negative log likelihood solutions implies minimum number of variants
+    minVariants = min(map(len,all_solutions))
+    if len(min_sol) == minVariants:
+        minSizeOpt_count +=1
         
-        #If there is one solution among all optimal which matches true variants, assign to var_predicted
-        for solution in all_solutions:
-            if set(solution) == set(variants_current):
-                var_predicted = solution
-                break
+    #Count number of simulations where solution with minimum negative log likelihood contains true variants
+    for sol in min_sol_list:
+        if len(set(variants_current) - set(sol)) == 0:
+            minNegLogLike_hasTrue += 1
+            break
         
-        Precision.append(precision(var_predicted, variants_current))
-        Recall.append(recall(var_predicted, variants_current))
-        pred_object_vals.append(pred_object_val)
-        df1 = df[var_predicted]
-        df2 = df.loc[reads_cov,variants_current]
-        df3 = df.loc[reads_cov,var_predicted]
-        prop = compute_proportions(df3)
-        pred_prop = create_dictionary(var_predicted, prop)
-        val = totalVariationDist(pred_prop, true_prop)
-        total_var.append(val)
-        print "======================================== SIMULATION " + str(x) + " ====================================================" +  "\n"
-        print 'True variants are:', variants_current, "\n"
-        print 'Predicted variants are:', var_predicted, "\n"
-        print 'True proportions are:', true_prop, "\n"
-        print 'Predicted proportions are:', pred_prop, "\n"
-#        print 'Solution(s):', all_solutions, "\n"
-        
-        #Likelihood approach
-        score_list = list()
-        min_score = sys.maxint
-        print("Number of optimal solutions: {}".format(len(all_solutions)))
-        
-        #compute negative log likelihood score for each solution
-        for i in range(len(all_solutions)):
-            print("Solution:{}".format(all_solutions[i]))
-            print("Objective value: {}".format(all_objective[i]))
-            print("Proportions:{}".format(compute_proportions(df.loc[reads_cov, all_solutions[i]])))
-            score = compute_likelihood(df.loc[reads_cov, all_solutions[i]])
-            score_list.append(score)
-            
-            if score <= min_score:
-                min_score = score
-            
-            print("\nNegative log likelihood score:{}\n".format(score))
-        
-        #identify those solutions which have minimum negative log likelihood
-        min_sol_list = [all_solutions[i] for i in range(len(all_solutions)) if score_list[i] == min_score]
-        min_sol = min_sol_list[0]
-        
-        #Observe whether minimum negative log likelihood really gives the true variants
-        for sol in min_sol_list:
-            if set(sol) == set(variants_current):
-                minNegLogLike_correct += 1
-                min_sol = sol
-                break
-        
-        #Observe whether minimum negative log likelihood solutions implies minimum number of variants
-        minVariants = min(map(len,all_solutions))
-        if len(min_sol) == minVariants:
-            minSizeOpt_count +=1
-            
-        #Count number of simulations where solution with minimum negative log likelihood contains true variants
-        for sol in min_sol_list:
-            if len(set(variants_current) - set(sol)) == 0:
-                minNegLogLike_hasTrue += 1
-                break
-            
-        print("Negative log likelihood score list:{}".format(score_list))
-        print("The solution which has minimum negative log likelihood is {0} and its score:{1}".format(min_sol, min_score))
-        
-        true_Objective_val = compute_True_objective_val(df2)
-        true_Objective_vals.append(true_Objective_val)
-        diff_obj_vals.append(Compute_obj_diff(pred_object_val,true_Objective_val))
-        
-        if predictedCorrectly(var_predicted, variants_current):
-                count += 1
-                bool_list.append(True)
-        else:
-                bool_list.append(False)
+    print("Negative log likelihood score list:{}".format(score_list))
+    print("The solution which has minimum negative log likelihood is {0} and its score:{1}".format(min_sol, min_score))
+    
+    true_Objective_val = compute_True_objective_val(df2)
+    true_Objective_vals.append(true_Objective_val)
+    diff_obj_vals.append(Compute_obj_diff(pred_object_val,true_Objective_val))
+    
+    if predictedCorrectly(var_predicted, variants_current):
+            count += 1
+            bool_list.append(True)
+    else:
+            bool_list.append(False)
 
 
                 
@@ -418,10 +420,11 @@ print("Number of simulations where solution with minimum negative log likelihood
 X = []
 os.system("rm {0}_*.fa".format(gene))
 
-for i in range(1,args["numOfIter"]):
+'''for i in range(1,args["numOfIter"]):
         X.append(i)
 plt.hist(total_var, bins='auto')
 plt.xlabel('Total Variation Distance in percentage')
 plt.ylabel('Frequency')
 #plt.show()
 plt.savefig("/home/glgan/Documents/Borrelia/{0}/{1}_totalVarDist".format(args["outputFolder"], gene))
+'''
