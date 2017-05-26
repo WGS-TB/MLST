ca8bebf0d1cc710e1326cff24fdf53c7bf85d79a
Stanley Gan <glgan@sfu.ca>
2017-05-25 18:40:16 -0700

    outputfolder path instead of just folder name

--- scripts/produceStats.sh
+++ scripts/produceStats.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sample=/home/glgan/Documents/Borrelia/Test_Data/SRR2034333_old/
 files=`ls $sample`
+outputFolderPath=$1
 
 for f in $files
 do
 	echo $f
 	cd $sample$f
-	python ~/Documents/Borrelia/scripts/Compute_values.py -g $f -l 40 -o $1 > ~/Documents/Borrelia/$1/$f\_output_stats.txt 
+	rm *.out *reads.txt *.fa
+	python ~/Documents/Borrelia/scripts/Compute_values.py -g $f -l 40 -o $outputFolderPath > $outputFolderPath$f\_output_stats.txt 
 done	
