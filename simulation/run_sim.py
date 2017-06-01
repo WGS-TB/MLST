004413551b7ee0aefb51476a04d8674ae392d07a
Stanley Gan <glgan@sfu.ca>
2017-06-01 13:40:09 -0700

    Added proportion calculation method parameter

--- simulation/run_sim.py
+++ simulation/run_sim.py
@@ -17,6 +17,7 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--numOfIter", required = False, default = 40, type=int)
 ap.add_argument("-f", "--simulationResultFolder", required=False, default="simulation_results")
 ap.add_argument("-c", "--coverage", required=False, default=30,type=int)
+ap.add_argument("-p", "--proportionMethod", required=True)
 args = vars(ap.parse_args())
 
 #Make directory for simulation results
@@ -52,10 +53,11 @@ for locus in genes:
         print("==== Removed previous .fa files ====")
     
     #Function to run simulation imported    
-    sim.simulation(locus,args["numOfIter"],originalPath, args["simulationResultFolder"], args["coverage"])
+    sim.simulation(locus,args["numOfIter"],originalPath, args["simulationResultFolder"], args["coverage"], args["proportionMethod"])
     sys.stdout = originalSTDOut
     
     os.chdir("..")
+    print("")
 
 #Summarize the results for all genes
 os.chdir("{0}/{1}/".format(originalPath, args["simulationResultFolder"]))
