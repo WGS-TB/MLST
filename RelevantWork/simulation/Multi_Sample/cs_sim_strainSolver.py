f28ccfbcaa4f3301c6d453771169d9a1f3db7edd
Parham Ghasemloo <parhamghasemloo75@gmail.com>
2019-01-10 14:29:57 +0330

    replaced infinite norm with two norm

--- RelevantWork/simulation/Multi_Sample/cs_sim_strainSolver.py
+++ RelevantWork/simulation/Multi_Sample/cs_sim_strainSolver.py
@@ -2,6 +2,7 @@ from __future__ import division
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import cplex
 from cplex.exceptions import CplexError
 from pipeline_functions import *
 from utility_functions import *
@@ -44,7 +45,70 @@ def defineProblem(cols, dims):
 # ----------------------------------------------------------------------
 
 
-def solve_cs(A, b, cols, epsilon=0.01, obj=None):
+def solve_cs_qcp(A, b, cols, epsilon, obj):
+    ub = [1.0 for x in range(cols)]
+    lb = [0.0 for x in range(cols)]
+
+    #---------Constructing matrices of the quadratic constraint---------
+    A_t = map(list, zip(*A))
+    m = len(A)
+    n = len(A_t)
+    cnames = ['x' + str(x) for x in range(cols)]
+
+    # Q : quadratic expression, which in our problem is A_t*A
+    A_t_A = [[],[],[]] # matrix in index-index-value format used by CPLEX
+    for i in range(n):
+	row = []
+	for j in range(n):
+	    val = 0
+	    for k in range(m):
+		val += A_t[i][k] * A[k][j]
+	    row.append(val)
+	    A_t_A[0].append(i)
+	    A_t_A[1].append(j)
+	    A_t_A[2].append(val)
+
+    # L : linear expression, which in our problem is 2(A_t*b)_t
+    A_t_b = [[],[]] # vector in index-value format used by CPLEX
+    for i in range(n):
+	val = 0
+	for j in range(m):
+	    val += -2 * A_t[i][j] * b[j]
+	A_t_b[0].append(i)
+	A_t_b[1].append(val)
+
+    # RHS : righthand side, which in our problem is epsilon - ||b||^2
+    b_norm2 = 0
+    for i in range(len(b)):
+	b_norm2 += b[i] * b[i]
+    RHS = epsilon - b_norm2
+
+    try:
+        prob = cplex.Cplex()
+        prob.objective.set_sense(prob.objective.sense.minimize)
+        prob.variables.add(obj=obj, ub=ub, lb=lb, names=cnames)
+        prob.quadratic_constraints.add(name="error",
+                                lin_expr=A_t_b,
+                                quad_expr=A_t_A,
+                                rhs=RHS,
+                                sense="L")
+
+        prob.solve()
+
+        print "Solution status:", prob.solution.get_status(), prob.solution.status[prob.solution.get_status()]
+
+        x = prob.solution.get_values()
+	print prob.solution.get_objective_value()
+        error = np.sum(np.subtract(np.dot(A, x), b))
+        return x, sum(x), error
+
+    except CplexError, exc:
+        print exc
+        print "\nERROR: cplex could not solve the single sample problem with changed proportions of previous step\n"
+	return [0 for _ in range(len(b))], 0, np.inf
+
+
+def solve_cs(A, b, cols, epsilon=0.01, obj=None, qcp=False):
     print "----------------------------------------------------------------------\n                     Solving the CS " \
           "using CPLEX\n---------------------------------------------------------------------- "
     print
@@ -53,6 +117,10 @@ def solve_cs(A, b, cols, epsilon=0.01, obj=None):
 
     if obj == None:
 	obj = [1.0 for x in range(cols)]
+
+    if qcp:
+	return solve_cs_qcp(A, b, cols, epsilon, obj)
+
     ub = [1.0 for x in range(cols)]
     lb = [0.0 for x in range(cols)]
     cnames = ['x' + str(x) for x in range(cols)]
@@ -197,7 +265,7 @@ def single_sample_solver(loci, varAndProp, reference, strains, eps, re=False, br
     print np.array(A).shape
 
     # Now we solve the cs problem
-    strainProps, sumOfProps, error = solve_cs(A, b, noc, eps, weights)
+    strainProps, sumOfProps, error = solve_cs(A, b, noc, eps, weights, True)
     print '~~~', sumOfProps, error
 
     strainNums = np.nonzero(strainProps)[0]
